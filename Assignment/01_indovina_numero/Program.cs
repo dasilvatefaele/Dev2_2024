08961d1525655f21fe581c28fe1a9766631d1939
Felipe <dasilvatefaele@gmail.com>
2024-10-18 13:00:54 +0200

    indovina_numerov_v1.0

---
+++ Assignment/01_indovina_numero/Program.cs
@@ -0,0 +1,42 @@
+﻿/******************************************************************************/
+//                            INDOVINA NUMERO v1.0
+/******************************************************************************/
+
+Console.Clear();
+Random random = new Random();
+//new è il costruttore della classe Random(); che istanzia un oggetto Random
+//random è l'oggetto Random che possiamo utilizzare per generare numeri casuali
+
+int numeroDaIndovinare = random.Next(1,101); // Next è il metodo che genera un numero casuale
+//l'intervallo del metodo .Next è semi aperto tra 1 e 101 
+//comprende il numero iniziale 1 ma esclude quello finale 101
+//dunque l'intervallo è da 1 a 100
+
+//verifico numero - OK
+//Console.WriteLine(numeroDaIndovinare);
+int numeroTentativi = 10;
+Console.Write($"Ho in mente un numero... Indovinalo! Hai {numeroTentativi} tentativi --> ");
+
+while(true){
+    int numeroUtente = int.Parse(Console.ReadLine());
+//  int numeroUtente = Convert.ToInt32(Console.ReadLine()); conversione alternativa
+    if (numeroUtente == numeroDaIndovinare)
+    {
+        Console.WriteLine($"Hai indovinato! Avevo pensato proprio il {numeroDaIndovinare}");
+        break;
+    }else{
+        numeroTentativi--;
+        Console.WriteLine($"Mmm... Sbagliato :( Ora hai {numeroTentativi} tentativi...");
+        Console.Write("--> ");
+        
+        if (numeroTentativi == 0)
+        {
+            Console.Write($"Mi dispiace... Il numero che avevo pensato era {numeroDaIndovinare}\n\n");
+            break;
+        }
+    }
+}
+
+
+
+
---
+++ esercitazioni/A_Prove/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int[] array3 = { 1, 2, 3, 4, 5 }; // dichiara e inizializza un array di interi
+Array.Clear(array3, 0, 5); // cancella gli elementi di array3 da 0 a array3.Length
+Console.WriteLine($"Lunghezza Array {array3.Length}"); // 5
+
+// lunghezza array = 5, ma gli indici sono 0, 1, 2, 3, 4 
+// quindi Array.Clear(array3, 0, array3.Length);   gli argomenti sono ([nome_array],[index di inizio],[???QUANTO CANCELLARE])
+
+Console.WriteLine(string.Join(", ", array3));
+
+
+// LISTA - ESEMPIO DI METODO TRIMEXCESS
+// riduce la capacita di una lista al numero di elementi presenti
+List<int> lista12 = new List<int> { 1, 2, 3, 4, 5 }; // dichiara e inizializza una lista di interi
+
+Console.WriteLine(lista12.Capacity); // stampa la capacita di lista12
+
+// La capacità non è una caratteristica degli ARRAY dato che la lunghezza della lista è variabile?
+// La lista ha 5 elementi ma capacità 8 [???]
+
+
+lista12.TrimExcess(); // riduce la capacita di lista12 al numero di elementi presenti
+Console.WriteLine(lista12.Capacity); // stampa la capacita di lista12
