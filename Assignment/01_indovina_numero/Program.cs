971dd1524879865bb99c6535b147f12ecf5c42a6
Felipe <dasilvatefaele@gmail.com>
2024-10-22 13:58:45 +0200

    Indovina Numero v1.4b - commenti migliorati

--- Assignment/01_indovina_numero/Program.cs
+++ Assignment/01_indovina_numero/Program.cs
@@ -1,11 +1,11 @@
 ﻿/******************************************************************************/
-//                            INDOVINA NUMERO v1.3 + v1.4
+//                            INDOVINA NUMERO v1.4b
 /******************************************************************************/
 // v1.1 : pulizia codice, console più pulita, formattazione più omogenea
 // v1.2 : possibilità di scegliere Facile (1-20), Medio (1-50), Difficile (1-100)
 // v1.3 : dopo 5 tentativi sbagliati propone 3 indizi 
 //        maggiore o minore / pari o dispari /
-// v1.4 : Acqua (differenza > 50%) fuoco (differenza < 25%) fuochissmo (differenza < 10%)
+// v1.4 : Oceano (differenza > 50%), Acqua (25% > differenza < 50% )  Fuoco (differenza < 25%) Fuochissmo (differenza < 10%)
 
 using System.Security.Principal;
 
@@ -42,21 +42,21 @@ do{
         case 1:
             sceltaValida = true;
             numeroDaIndovinare = random.Next(1,21); 
-            intervallo = 19;
+            intervallo = 19; // da 1 a 20
             Console.Clear();
             Console.WriteLine("Modalità: Facile (penso a un numero tra 1 e 20)");
         break;
         case 2:
             sceltaValida = true;
             numeroDaIndovinare = random.Next(1,51); 
-            intervallo = 49;
+            intervallo = 49; // da 1 a 50
             Console.Clear();
             Console.WriteLine("Modalità: Medio (penso a un numero tra 1 e 50)");
         break;
         case 3:
             sceltaValida = true;
             numeroDaIndovinare = random.Next(1,101); 
-            intervallo = 99;
+            intervallo = 99; // da 1 a 100
             Console.Clear();
             Console.WriteLine("Modalità: Difficile (penso a un numero tra 1 e 100)");
         break;  
@@ -71,9 +71,9 @@ do{
 //Stampa istruzioni di gioco
 Console.WriteLine("Istruzioni di gioco: Hai 10 tentativi per indovinare un numero che ho pensato... Cominciamo!\n");
 Console.WriteLine("Premere un tasto per iniziare...");
-//Console.ReadKey();
+Console.ReadKey();
 
-// Dichiarazione e inizializzazione
+// Dichiarazione e inizializzazione tentativi
 int numeroTentativi = 10;
 
 //Inizio sessione di gioco
@@ -83,10 +83,11 @@ Console.Write($"Ho in mente un numero... Indovinalo! Hai {numeroTentativi} tenta
 while(true){
     int numeroUtente = int.Parse(Console.ReadLine()); // Acquisizione 
 
-    if (numeroUtente == numeroDaIndovinare){
+    // confronto numeri
+    if (numeroUtente == numeroDaIndovinare){ // "Hai indovinato!" - Fine sessione con vittoria
         Console.WriteLine($"\nHai indovinato! Avevo pensato proprio al {numeroDaIndovinare}\n");
         break;
-    } else {    // <----- La sessione di gioco si svolge qui
+    } else {    // <----- "Sbagliato :(" - La sessione di gioco si svolge qui 
         
         numeroTentativi--; // Decremento tentativi
 
@@ -95,27 +96,28 @@ while(true){
 
         int diffNumero = Math.Abs(numeroDaIndovinare - numeroUtente);
 
-        if (diffNumero > intervallo / 2){
+        if (diffNumero > intervallo / 2){ // differenza > 50% dell'intervallo (molto distanti)
             Console.WriteLine("Indizio: Oceano!");
-        } else if (diffNumero < intervallo / 2 && diffNumero > intervallo / 4 ){
-            Console.WriteLine("Indizio: Acqua!");
-        } else if (diffNumero < intervallo / 4){
+        } else if (diffNumero < intervallo / 2 && diffNumero > intervallo / 4 ){ // differenza tra 50%  e 25% dell'intervallo (distanti)
+            Console.WriteLine("Indizio: Acqua!"); 
+        } else if (diffNumero < intervallo / 4){ // differenza < 25% intervallo (vicini)
             Console.WriteLine("Indizio: Fuoco!");
-        }else if (diffNumero < intervallo / 10){
+        }else if (diffNumero < intervallo / 10){ // differenza < 10% intervallo (molto vicini)
             Console.WriteLine("Indizio: Fuochissimo!");
         }
         
-        if (numeroTentativi == 5) { // Indizio 5
+        if (numeroTentativi == 5) { // Indizio dopo 5 tentativi
             if (numeroDaIndovinare % 2 == 0) {{Console.WriteLine("Indizio: il numero che ho pensato è pari... :)");}}
             else {Console.WriteLine("Indizio: il numero che ho pensato è dispari... :)");}
         }
 
-        if (numeroTentativi < 5 && numeroTentativi > 0){ // Indizio 4, 3, 2, 1
+        if (numeroTentativi < 5 && numeroTentativi > 0){ // Indizio al tentativo 4, 3, 2, 1
             if (numeroUtente > numeroDaIndovinare) {Console.WriteLine("Indizio: il numero che ho pensato è più basso... :)");}
             if (numeroUtente < numeroDaIndovinare) {Console.WriteLine("Indizio: il numero che ho pensato è più alto... :)");}
         }
         
-        if (numeroTentativi == 0){  // Fine sessione di gioco
+        // Fine sessione di gioco con sconfitta
+        if (numeroTentativi == 0){  
             Console.Clear();
             Console.Write($"Mi dispiace... Il numero che avevo pensato era {numeroDaIndovinare}\n\n");
             break;
