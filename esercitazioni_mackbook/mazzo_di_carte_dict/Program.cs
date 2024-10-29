fe7a64b9f80797edd2640d768602ecc36dd00113
dasilvatefaele <dasilvatefaele@gmail.com>
2024-10-29 13:15:37 +0100

    Studio dei Dizionari - mazzo_di_carte_dict con array (working!)

--- esercitazioni_mackbook/mazzo_di_carte_dict/Program.cs
+++ esercitazioni_mackbook/mazzo_di_carte_dict/Program.cs
@@ -1,59 +1,42 @@
 ﻿using System.Collections;
 using System.Dynamic;
 
-void CustomDebug(){
-    Console.WriteLine("CustomDebug: Premi per continuare l'esecuzione...");
- //   Console.ReadKey();
+Console.Clear();
+
+void CustomDebug() // funzione per mettere in pausa l'esecuzione
+{
+    Console.WriteLine("Premi per continuare l'esecuzione...");
+    Console.ReadKey();
     return;
 }
 
-Dictionary<int,string> diCuori = new Dictionary<int,string>();
-Dictionary<int,string> diDenari = new Dictionary<int,string>();
-Dictionary<int,string> diPicche = new Dictionary<int,string>();
-Dictionary<int,string> diFiori = new Dictionary<int,string>();
-List<Dictionary<int,string>> mazzoDiCarte = new List<Dictionary<int,string>>();
+// Dichiarazione:
+Dictionary<int, string>[] mazzoDiCarte = new Dictionary<int, string>[4];
 
-Console.Clear();
+//inizializzazione:
+mazzoDiCarte[0] = new Dictionary<int, string>();  // Cuori
+mazzoDiCarte[1] = new Dictionary<int, string>();  // Denari
+mazzoDiCarte[2] = new Dictionary<int, string>();  // Picche
+mazzoDiCarte[3] = new Dictionary<int, string>();  // Fiori
 
-//inizializzazione array carte
-int[] numCarte = new int[13];
-for (int i = 1; i <= 13; i++){
-    numCarte[i-1] = i;
-}
-// Console.WriteLine(string.Join(", ", numCarte)); // Verifico inserimento valore carte (riuscito)
-
-// Inizializzazione per ogni seme
-foreach (int num in numCarte){
-    diCuori.Add(num, "di Cuori");
+// Assegnazione:
+for (int i = 1; i <= 13; i++)
+{
+    mazzoDiCarte[0].Add(i, "di Cuori");
+    mazzoDiCarte[1].Add(i, "di Denari");
+    mazzoDiCarte[2].Add(i, "di Picche");
+    mazzoDiCarte[3].Add(i, "di Fiori");
 }
 
-foreach (int num in numCarte){
-    diDenari.Add(num, "di Denari");
-}
-
-foreach (int num in numCarte){
-    diPicche.Add(num, "di Picche");
-}
-
-foreach (int num in numCarte){
-    diFiori.Add(num, "di Fiori");
-}
-
-// Inizializzazione Lista mazzoDiCarte
-mazzoDiCarte.Add(diCuori);
-mazzoDiCarte.Add(diFiori);
-mazzoDiCarte.Add(diDenari);
-mazzoDiCarte.Add(diPicche);
-
-// Verifico presenza delle carte in mazzoDiCarte // riuscito
-/*
-
-foreach (var seme in mazzoDiCarte){
-    foreach (var carta in seme){
-        Console.WriteLine($"{carta.Key} {carta.Value}");
+// Verifico presenza delle carte in mazzoDiCarte via stampa // riuscito
+foreach (var seme in mazzoDiCarte)
+{
+    foreach (var carta in seme)
+    {
+        Console.Write($"{carta.Key} {carta.Value}\t");
     }
+    Console.WriteLine("\n");
 }
-*/
 
 CustomDebug();
 
@@ -62,58 +45,87 @@ int contatoreMazzo = 1; // da incrementare dentro il ciclo
 Random random = new Random();
 int pescaRandom;
 
-List<Dictionary<int,string>> manoGiocatore = new List<Dictionary<int,string>>(){};
-Dictionary<int,string> mano = new Dictionary<int,string>();
-string comando; 
+Dictionary<int, string>[] manoGiocatore = new Dictionary<int, string>[4];
+// Inizializza i dizionari in manoGiocatore
+manoGiocatore[0] = new Dictionary<int, string>();  // Cuori
+manoGiocatore[1] = new Dictionary<int, string>();  // Denari
+manoGiocatore[2] = new Dictionary<int, string>();  // Picche
+manoGiocatore[3] = new Dictionary<int, string>();  // Fiori
 
-do{
-    pescaRandom = random.Next(1,totCarte); // da 1 a 52
+string comando;
+
+do
+{
+    //inizializzazioni variabili
+    pescaRandom = random.Next(1, totCarte); // da 1 a 52
     contatoreMazzo = 1;
-    Console.WriteLine("Pesca una carta...");
-    //Console.ReadKey();
 
-    foreach (var seme in mazzoDiCarte){
-        foreach (var carta in seme){
-            if (contatoreMazzo == pescaRandom){
-                mano.Add(carta.Key, carta.Value);
-                manoGiocatore.Clear();
-                manoGiocatore.Add(mano);  
-                seme.Remove(carta.Key);
-                totCarte--;
+    Console.WriteLine("Pesca una carta...");
+    Console.ReadKey();
+
+    foreach (var seme in mazzoDiCarte)
+    {
+        foreach (var carta in seme)
+        {
+            if (contatoreMazzo == pescaRandom)
+            {
+                if (seme.ContainsValue("di Cuori"))
+                {
+                    manoGiocatore[0].Add(carta.Key, carta.Value);
+                }
+                else if (seme.ContainsValue("di Denari"))
+                {
+                    manoGiocatore[1].Add(carta.Key, carta.Value);
+                }
+                else if (seme.ContainsValue("di Picche"))
+                {
+                    manoGiocatore[2].Add(carta.Key, carta.Value);
+                }
+                else if (seme.ContainsValue("di Fiori"))
+                {
+                    manoGiocatore[3].Add(carta.Key, carta.Value);
+                }
+
+                seme.Remove(carta.Key); // rimuovi carta dal mazzo dopo averla copiata
+                totCarte--;  // decremento totale carte
                 break;
             }
-            
+
             contatoreMazzo++;
         }
-        if (contatoreMazzo == pescaRandom){break;}
+        if (contatoreMazzo == pescaRandom) { break; }
     }
 
+    //stampa Mano Giocatore
     Console.WriteLine("*** La tua mano: ***");
-    
-    foreach (var seme in manoGiocatore){
-        foreach (var cartaInMano in seme){
-            Console.WriteLine($"{cartaInMano.Key} {cartaInMano.Value}");
+    Console.WriteLine("\n");
+    foreach (var seme in manoGiocatore)
+    {
+        foreach (var carta in seme)
+        {
+            Console.WriteLine($"{carta.Key} {carta.Value}\t");
         }
+        // Console.WriteLine("\n");
     }
-
-
+    Console.WriteLine("\n");
+    //stampa carte restanti nel mazzo
     Console.WriteLine("*** Il mazzo: ***");
-    foreach (var seme in mazzoDiCarte){
-        foreach (var carta in seme){
+    foreach (var seme in mazzoDiCarte)
+    {
+        foreach (var carta in seme)
+        {
             Console.Write($"{carta.Key} {carta.Value}\t");
         }
         Console.WriteLine("\n");
     }
-    /*
-    .
-    .
-    .
-    */
+
+    //messaggi di dialogo
     Console.WriteLine("Debug Data *************");
-    Console.WriteLine($"Carte nel mazzo: {totCarte-1}");
+    Console.WriteLine($"Carte nel mazzo: {totCarte - 1}");
     Console.WriteLine("Debug Data *************");
     Console.WriteLine("Continui?");
+
     comando = Console.ReadLine();
     comando = comando.ToUpper();
 
-}while(comando != "EXIT");
+} while (comando != "EXIT");
--- esercitazioni_mackbook/mazzo_di_carte_dict/README.md
+++ esercitazioni_mackbook/mazzo_di_carte_dict/README.md
@@ -497,3 +497,190 @@ git add --all
 git commit -m "Studio dei Dizionari - mazzo_di_carte_dict"
 git push -u origin main
 ```
+
+## Nuovo approccio: Array di Dizionari
+E' possibile creare array di dizionari. Per ogni indice sarà dunque possibile utilizzare un seme diverso.
+
+>Esempio:
+```csharp
+// Dichiarazione:
+Dictionary<int, string>[] mazzoDiCarte = new Dictionary<int, string>[4];
+
+//inizializzazione:
+mazzoDiCarte[0] = new Dictionary<int, string>();  // Cuori
+mazzoDiCarte[1] = new Dictionary<int, string>();  // Denari
+mazzoDiCarte[2] = new Dictionary<int, string>();  // Picche
+mazzoDiCarte[3] = new Dictionary<int, string>();  // Fiori
+
+// Assegnazione:
+for (int i = 1; i <= 13; i++){
+    mazzoDiCarte[0].Add(i, "di Cuori");
+    mazzoDiCarte[1].Add(i, "di Denari");
+    mazzoDiCarte[2].Add(i, "di Picche");
+    mazzoDiCarte[3].Add(i, "di Fiori");
+}
+ ```
+
+ ### Considerazione:
+ Ora che è stata semplificata la struttura di dati, la stessa struttura va applicata alla mano del giocatore: 
+ ```csharp
+// Dichiarazione:
+Dictionary<int, string>[] mazzoDiCarte = new Dictionary<int, string>[4];
+
+//inizializzazione:
+mazzoDiCarte[0] = new Dictionary<int, string>();  // Cuori
+mazzoDiCarte[1] = new Dictionary<int, string>();  // Denari
+mazzoDiCarte[2] = new Dictionary<int, string>();  // Picche
+mazzoDiCarte[3] = new Dictionary<int, string>();  // Fiori
+ ```
+
+ >Esempio di codice (funzionante)
+ ```csharp
+using System.Collections;
+using System.Dynamic;
+
+Console.Clear();
+
+void CustomDebug() // funzione per mettere in pausa l'esecuzione
+{
+    Console.WriteLine("Premi per continuare l'esecuzione...");
+    Console.ReadKey();
+    return;
+}
+
+// Dichiarazione:
+Dictionary<int, string>[] mazzoDiCarte = new Dictionary<int, string>[4];
+
+//inizializzazione:
+mazzoDiCarte[0] = new Dictionary<int, string>();  // Cuori
+mazzoDiCarte[1] = new Dictionary<int, string>();  // Denari
+mazzoDiCarte[2] = new Dictionary<int, string>();  // Picche
+mazzoDiCarte[3] = new Dictionary<int, string>();  // Fiori
+
+// Assegnazione:
+for (int i = 1; i <= 13; i++)
+{
+    mazzoDiCarte[0].Add(i, "di Cuori");
+    mazzoDiCarte[1].Add(i, "di Denari");
+    mazzoDiCarte[2].Add(i, "di Picche");
+    mazzoDiCarte[3].Add(i, "di Fiori");
+}
+
+// Verifico presenza delle carte in mazzoDiCarte via stampa // riuscito
+foreach (var seme in mazzoDiCarte)
+{
+    foreach (var carta in seme)
+    {
+        Console.Write($"{carta.Key} {carta.Value}\t");
+    }
+    Console.WriteLine("\n");
+}
+
+CustomDebug();
+
+int totCarte = 53;
+int contatoreMazzo = 1; // da incrementare dentro il ciclo
+Random random = new Random();
+int pescaRandom;
+
+Dictionary<int, string>[] manoGiocatore = new Dictionary<int, string>[4];
+// Inizializza i dizionari in manoGiocatore
+manoGiocatore[0] = new Dictionary<int, string>();  // Cuori
+manoGiocatore[1] = new Dictionary<int, string>();  // Denari
+manoGiocatore[2] = new Dictionary<int, string>();  // Picche
+manoGiocatore[3] = new Dictionary<int, string>();  // Fiori
+
+string comando;
+
+do
+{
+    //inizializzazioni variabili
+    pescaRandom = random.Next(1, totCarte); // da 1 a 52
+    contatoreMazzo = 1;
+
+    Console.WriteLine("Pesca una carta...");
+    Console.ReadKey();
+
+    foreach (var seme in mazzoDiCarte)
+    {
+        foreach (var carta in seme)
+        {
+            if (contatoreMazzo == pescaRandom)
+            {
+                if (seme.ContainsValue("di Cuori"))
+                {
+                    manoGiocatore[0].Add(carta.Key, carta.Value);
+                }
+                else if (seme.ContainsValue("di Denari"))
+                {
+                    manoGiocatore[1].Add(carta.Key, carta.Value);
+                }
+                else if (seme.ContainsValue("di Picche"))
+                {
+                    manoGiocatore[2].Add(carta.Key, carta.Value);
+                }
+                else if (seme.ContainsValue("di Fiori"))
+                {
+                    manoGiocatore[3].Add(carta.Key, carta.Value);
+                }
+
+                seme.Remove(carta.Key); // rimuovi carta dal mazzo dopo averla copiata
+                totCarte--;  // decremento totale carte
+                break;
+            }
+
+            contatoreMazzo++;
+        }
+        if (contatoreMazzo == pescaRandom) { break; }
+    }
+
+    //stampa Mano Giocatore
+    Console.WriteLine("*** La tua mano: ***");
+    Console.WriteLine("\n");
+    foreach (var seme in manoGiocatore)
+    {
+        foreach (var carta in seme)
+        {
+            Console.WriteLine($"{carta.Key} {carta.Value}\t");
+        }
+        // Console.WriteLine("\n");
+    }
+    Console.WriteLine("\n");
+    //stampa carte restanti nel mazzo
+    Console.WriteLine("*** Il mazzo: ***");
+    foreach (var seme in mazzoDiCarte)
+    {
+        foreach (var carta in seme)
+        {
+            Console.Write($"{carta.Key} {carta.Value}\t");
+        }
+        Console.WriteLine("\n");
+    }
+
+    //messaggi di dialogo
+    Console.WriteLine("Debug Data *************");
+    Console.WriteLine($"Carte nel mazzo: {totCarte - 1}");
+    Console.WriteLine("Debug Data *************");
+    Console.WriteLine("Continui?");
+
+    comando = Console.ReadLine();
+    comando = comando.ToUpper();
+
+} while (comando != "EXIT");
+ ```
+
+### Bug Minore da risolvere:
+Può capitare che una pesca vada a vuoto, cioè nessuna carta viene pescata, le carte nel mazzo rimangono dello stesso numero, e nessuna carta viene aggiunta alla mano. In sostanza qualche volta viene eseguito un ciclo a vuoto. Ipotizzo sia per via della comparazione tra `pescaRandom` e `contatoreMazzo`. Da verificare
+
+## Prossimi obiettivi: 
+- 11: stampa "Jack"
+- 12: stampa "Regina"
+- 13: stampa "Re"
+- Risoluzione Bug Minore
+
+ > Comandi di versionamento:
+ ```bash
+git add --all
+git commit -m "Studio dei Dizionari - mazzo_di_carte_dict con array (working!)"
+git push -u origin main
+```
