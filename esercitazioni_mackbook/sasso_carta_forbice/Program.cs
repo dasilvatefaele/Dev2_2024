6354b8dde2ea3fc6648d8aa054ee68f4dbe2944d
dasilvatefaele <dasilvatefaele@gmail.com>
2024-11-07 11:15:36 +0100

    Sasso, Carta, Forbice (v1.1)

--- esercitazioni_mackbook/sasso_carta_forbice/Program.cs
+++ esercitazioni_mackbook/sasso_carta_forbice/Program.cs
@@ -5,94 +5,144 @@ Console.Clear();
 // Dichiarazione
 int numUser;
 int randomPC;
+int contatoreRound = 5;
+int dialogoRound = 1;
 string manoUser = "";
 string manoPC = "";
 Random random = new Random();
 
 
-// Dialogo
-Console.WriteLine("[1] SASSO\t[2] CARTA\t [3] FORBICE");
-Console.Write("> ");
-numUser = int.Parse(Console.ReadLine());
-
-
-// Controllo inserimento
-while (numUser != 1 && numUser != 2 && numUser != 3)
-{
-
-    Console.Clear();
-    Console.WriteLine("Inserimento non valido.");
-    Console.WriteLine("[1] SASSO\t[2] CARTA\t [3] FORBICE");
-    Console.Write("> ");
-    numUser = int.Parse(Console.ReadLine());
-    
-}
-
-
-// Assegnazione mano utente 
-switch (numUser)
-{
-
-    case 1:
-        manoUser = "SASSO";  
-    break;
-
-    case 2:
-        manoUser = "CARTA"; 
-    break;
-
-    case 3:
-        manoUser = "FORBICE";
-    break;
-
-}
-
-
-// Assegnazione mano PC
-randomPC = random.Next(1,4); //
-switch (randomPC)
-{
-
-    case 1:
-        manoPC = "SASSO";
-    break;
-
-    case 2:
-        manoPC = "CARTA"; 
-    break;
-
-    case 3:
-        manoPC = "FORBICE"; 
-    break;
-
-}
-
-
-// Dialogo
-Console.Clear();
-Console.WriteLine("'SASSO, CARTA, FORBICE!'");
-Console.WriteLine("Premi un tasto per giocare...");
+// Dialogo iniziale
+Console.WriteLine("*** SASSO, CARTA FORBICE! ***");
+Console.WriteLine("\nPremi un tasto per giocare...");
 Console.ReadKey();
 
 
-// Logica di comparazione
-if (manoUser == "CARTA" && manoPC == "SASSO" || manoUser == "FORBICE" && manoPC == "CARTA" || manoUser == "SASSO" && manoPC == "FORBICE")
+do
 {
 
-    // Vittoria
+    // Dialogo Round
     Console.Clear();
-    Console.WriteLine($"Tu\t\tAvversario");
-    Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
-    Console.WriteLine("Hai vinto!\n");
+    Console.WriteLine($"Round {dialogoRound}\n");
 
-} 
-else 
-{
 
-    // Sconfitta
+    // Dialogo inserimento
+    Console.WriteLine("[1] SASSO\t[2] CARTA\t [3] FORBICE");
+    Console.Write("> ");
+    numUser = int.Parse(Console.ReadLine());
+
+
+    // Controllo inserimento
+    while (numUser != 1 && numUser != 2 && numUser != 3)
+    {
+
+        Console.Clear();
+        Console.WriteLine("Inserimento non valido.");
+        Console.WriteLine("[1] SASSO\t[2] CARTA\t [3] FORBICE");
+        Console.Write("> ");
+        numUser = int.Parse(Console.ReadLine());
+        
+    }
+
+
+    // Assegnazione mano utente 
+    switch (numUser)
+    {
+
+        case 1:
+            manoUser = "SASSO";  
+        break;
+
+        case 2:
+            manoUser = "CARTA"; 
+        break;
+
+        case 3:
+            manoUser = "FORBICE";
+        break;
+
+    }
+
+
+    // Assegnazione mano PC
+    randomPC = random.Next(1,4); //
+    switch (randomPC)
+    {
+
+        case 1:
+            manoPC = "SASSO";
+        break;
+
+        case 2:
+            manoPC = "CARTA"; 
+        break;
+
+        case 3:
+            manoPC = "FORBICE"; 
+        break;
+
+    }
+
+
+    // Dialogo
     Console.Clear();
-    Console.WriteLine($"Tu\t\tAvversario");
-    Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
-    Console.WriteLine("Hai perso!\n");
+    Console.WriteLine("'SASSO, CARTA, FORBICE!'");
+    Console.WriteLine("\nPremi un tasto per giocare...");
+    Console.ReadKey();
 
-}
+
+    // Logica di comparazione
+    if (manoUser == "CARTA" && manoPC == "SASSO" || manoUser == "FORBICE" && manoPC == "CARTA" || manoUser == "SASSO" && manoPC == "FORBICE")
+    {
+
+        // Vittoria
+        Console.Clear();
+        Console.WriteLine($"Tu\t\tAvversario");
+        Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
+        Console.WriteLine("Hai vinto!\n");
+
+    }
+    else if (manoUser == manoPC)
+    {
+
+        // Pareggio
+        Console.Clear();
+        Console.WriteLine($"Tu\t\tAvversario");
+        Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
+        Console.WriteLine("Pareggio!\n");
+
+    } 
+    else 
+    {
+
+        // Sconfitta
+        Console.Clear();
+        Console.WriteLine($"Tu\t\tAvversario");
+        Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
+        Console.WriteLine("Hai perso!\n");
+
+    }
+
+
+    // Ometti dialogo "Continua" quando c'è l'ultimo Round
+    if (contatoreRound-1 != 0)
+    {
+
+        // Dialogo
+        Console.WriteLine("Continua...");
+        Console.ReadKey();
+
+    }
+
+
+    // Decremento contatore
+    contatoreRound--;
+
+    // Incremento dialogoRound
+    dialogoRound++;
+
+
+} while (contatoreRound != 0);
+
+
+Console.WriteLine("Fine partita!\n");
--- esercitazioni_mackbook/sasso_carta_forbice/README.md
+++ esercitazioni_mackbook/sasso_carta_forbice/README.md
@@ -81,7 +81,7 @@ git push -u origin main
 - Commenti
 
 <details>
-<summary>Versione 1.09</summmart>
+<summary>Versione 1.09</summary>
 
 ```csharp
 // Pulizia Console
@@ -199,6 +199,164 @@ git push -u origin main
 - Inserire il gioco in un ciclo di 5 round `do{}while(contatoreRound != 0)`
     - Creare `int contatoreRound = 5`
     - `contatoreRound--` alla fine di ogni ciclo.
+
+
+<details>
+<summary>Versione 1.1</summary>
+
+```csharp
+// Pulizia Console
+Console.Clear();
+
+
+// Dichiarazione
+int numUser;
+int randomPC;
+int contatoreRound = 5;
+int dialogoRound = 1;
+string manoUser = "";
+string manoPC = "";
+Random random = new Random();
+
+
+// Dialogo iniziale
+Console.WriteLine("*** SASSO, CARTA FORBICE! ***");
+Console.WriteLine("\nPremi un tasto per giocare...");
+Console.ReadKey();
+
+
+do
+{
+
+    // Dialogo Round
+    Console.Clear();
+    Console.WriteLine($"Round {dialogoRound}\n");
+
+
+    // Dialogo inserimento
+    Console.WriteLine("[1] SASSO\t[2] CARTA\t [3] FORBICE");
+    Console.Write("> ");
+    numUser = int.Parse(Console.ReadLine());
+
+
+    // Controllo inserimento
+    while (numUser != 1 && numUser != 2 && numUser != 3)
+    {
+
+        Console.Clear();
+        Console.WriteLine("Inserimento non valido.");
+        Console.WriteLine("[1] SASSO\t[2] CARTA\t [3] FORBICE");
+        Console.Write("> ");
+        numUser = int.Parse(Console.ReadLine());
+        
+    }
+
+
+    // Assegnazione mano utente 
+    switch (numUser)
+    {
+
+        case 1:
+            manoUser = "SASSO";  
+        break;
+
+        case 2:
+            manoUser = "CARTA"; 
+        break;
+
+        case 3:
+            manoUser = "FORBICE";
+        break;
+
+    }
+
+
+    // Assegnazione mano PC
+    randomPC = random.Next(1,4); //
+    switch (randomPC)
+    {
+
+        case 1:
+            manoPC = "SASSO";
+        break;
+
+        case 2:
+            manoPC = "CARTA"; 
+        break;
+
+        case 3:
+            manoPC = "FORBICE"; 
+        break;
+
+    }
+
+
+    // Dialogo
+    Console.Clear();
+    Console.WriteLine("'SASSO, CARTA, FORBICE!'");
+    Console.WriteLine("\nPremi un tasto per giocare...");
+    Console.ReadKey();
+
+
+    // Logica di comparazione
+    if (manoUser == "CARTA" && manoPC == "SASSO" || manoUser == "FORBICE" && manoPC == "CARTA" || manoUser == "SASSO" && manoPC == "FORBICE")
+    {
+
+        // Vittoria
+        Console.Clear();
+        Console.WriteLine($"Tu\t\tAvversario");
+        Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
+        Console.WriteLine("Hai vinto!\n");
+
+    }
+    else if (manoUser == manoPC)
+    {
+
+        // Pareggio
+        Console.Clear();
+        Console.WriteLine($"Tu\t\tAvversario");
+        Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
+        Console.WriteLine("Pareggio!\n");
+
+    } 
+    else 
+    {
+
+        // Sconfitta
+        Console.Clear();
+        Console.WriteLine($"Tu\t\tAvversario");
+        Console.WriteLine($"{manoUser}\t\t{manoPC}\n");
+        Console.WriteLine("Hai perso!\n");
+
+    }
+
+
+    // Ometti dialogo "Continua" quando c'è l'ultimo Round
+    if (contatoreRound-1 != 0)
+    {
+
+        // Dialogo
+        Console.WriteLine("Continua...");
+        Console.ReadKey();
+
+    }
+
+
+    // Decremento contatore
+    contatoreRound--;
+
+    // Incremento dialogoRound
+    dialogoRound++;
+
+
+} while (contatoreRound != 0);
+
+
+Console.WriteLine("Fine partita!\n");
+```
+
+</details>
+
 > Comandi di versionamento:
 ``` powershell
 git add --all
