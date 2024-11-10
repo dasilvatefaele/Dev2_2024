aae6c8fcc7270381d26e5ba19a5cf864681c2359
dasilvatefaele <dasilvatefaele@gmail.com>
2024-11-10 21:28:22 +0100

    Arkanoid in Raylib

---
+++ esercitazioni_mackbook/LeetCode/3133 Minimum Array End/Program.cs
@@ -0,0 +1,16 @@
+﻿//public class Solution {
+//    public long MinEnd(int n, int x) {
+
+        int n = 3;
+        int x = 4;
+
+        int[]nums=new int[n];
+        for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i]=x+i;
+        }
+
+//        return nums[n-1];
+
+//    }
+//}
