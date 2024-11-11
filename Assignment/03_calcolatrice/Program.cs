7b631be95622f5120eaf198e3b713baf50d27213
Felipe <dasilvatefaele@gmail.com>
2024-11-11 13:02:47 +0100

    03 calcolatrice v2

--- Assignment/03_calcolatrice/Program.cs
+++ Assignment/03_calcolatrice/Program.cs
@@ -1,26 +1,74 @@
-﻿//* chiedi all'utente di inserire due numeri
-double numero1;
-double numero2;
+﻿double numero1 = 0;
+double numero2 = 0;
 double risultato = -1;
+bool numeroConvertito = true;
+bool operatoreValido = true;
+bool numero2Valido = true;
+char operatore = ' ';
 
 //Dialogo
 Console.Clear();
 Console.WriteLine("Inserisci numeri:");
-Console.Write("> ");
-numero1 = double.Parse(Console.ReadLine());
-Console.Write("> ");
-numero2 = double.Parse(Console.ReadLine());
 
-//* chiedi all'utente di selezionare operatore matematico
-Console.WriteLine("Scegli l'operatore:");
-Console.WriteLine("\n+\t-\t*\t/\n");
-Console.Write("> ");
-char operatore = Console.ReadKey().KeyChar; 
-// acquisizione char singolo, non richiede l'invio per l'inserimento
-Console.WriteLine(); 
-// a capo
+// chiedi all'utente di inserire due numeri
+//! controllo: numero1
+//* che sia un numero con try-catch / do-while
+do
+{
+    Console.Write("n1 => ");
+    try
+    {
+        numero1 = double.Parse(Console.ReadLine());
+        numeroConvertito = true;
+    }
+    catch (FormatException e) // ? andava bene anche (Exception e) generico?
+    {
+        Console.WriteLine("Errore: inserire valore valido");
+        numeroConvertito = false;
+    }
+}while(!numeroConvertito);
 
-//* esegui l'operazione selezionata
+//! controllo: numero2
+//* che sia un numero con try-catch / do-while
+do
+{
+    Console.Write("n2 => ");
+    try
+    {
+        numero2 = double.Parse(Console.ReadLine());
+        numeroConvertito = true;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("Errore: inserire valore valido");
+        numeroConvertito = false;
+    }
+}while(!numeroConvertito);
+
+// chiedi all'utente di selezionare operatore matematico
+//! controllo: char operatore
+//* inserimento operatore con if-else/do-while
+do
+{
+    Console.WriteLine("Scegli l'operatore:");
+    Console.WriteLine("\n+\t-\t*\t/\n");
+    Console.Write("> ");
+    operatore = Console.ReadKey().KeyChar; 
+    if (operatore != '*' && operatore != '+' && operatore != '-' && operatore != '/')
+    {
+        Console.WriteLine();
+        Console.WriteLine("Errore: selezionare operatore valido");
+        operatoreValido = false;
+    }
+    else
+    {
+        operatoreValido = true;
+    }
+} while (!operatoreValido);
+
+Console.WriteLine(); // a capo
+
+// esegui l'operazione selezionata
 switch (operatore)
 {
     case '+':
@@ -33,12 +81,43 @@ switch (operatore)
         risultato = numero1*numero2;
         break;
     case '/':
-        risultato = numero1/numero2;
+        //! controllo: double numero2
+        //*     if == 0 --> NUMERO NON VALIDO, INSERISCI DI NUOVO FINCHé != 0
+        //*     else    --> NUMERO VALIDO, ESEGUI OPERAZIONE
+        do
+        {
+            if (numero2 == 0)
+            {
+                Console.WriteLine("Errore: Divisione per 0 non consentita");
+                numero2Valido = false;
+                //! controllo: double numero2
+                //* reinserimento e check che sia un numero con try-catch / do-while
+                do
+                {
+                    Console.Write("n2 => ");
+                    try
+                    {
+                        numero2 = double.Parse(Console.ReadLine());
+                        numeroConvertito = true;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Errore: inserire valore valido");
+                        numeroConvertito = false;
+                    }
+                }while(!numeroConvertito);
+            }
+            else
+            {
+                risultato = numero1/numero2;
+                numero2Valido = true;
+            }
+        } while (!numero2Valido);
         break;
     default:
         Console.WriteLine("Operatore non valido");
         break;
 }
 
-//* stampa risultato
+// stampa risultato
 Console.WriteLine($"= {risultato}");
