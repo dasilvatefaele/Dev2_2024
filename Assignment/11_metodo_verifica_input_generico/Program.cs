d18202797fee096eacd3b13fb7a8e18f5e51edce
dasilvatefaele <dasilvatefaele@gmail.com>
2024-11-24 18:29:46 +0100

    Verifica input generico

--- Assignment/11_metodo_verifica_input_generico/Program.cs
+++ Assignment/11_metodo_verifica_input_generico/Program.cs
@@ -1,15 +1,19 @@
-﻿
-int     inputSceltaMenu         = NumberInRange (1,8);
-int     inputNumeroIntero       = InputInt();
-double  inputNumeroConLaVirgola = InputDouble();
-string  inputfrase              = Inserimentofrase();
+﻿/***************************************************************
+                    FUNZIONI DI INSERIMENTO
+***************************************************************/
 
-Console.WriteLine ($"Input nel range: {inputSceltaMenu}");
-Console.WriteLine ($"input numero intero: {inputNumeroIntero}");
-Console.WriteLine ($"Input con la virgola: {inputNumeroConLaVirgola:F2}");
-Console.WriteLine ($"Input frase: {inputfrase}");
+Console.Clear();
+int inputSceltaMenu = NumberInRange(1, 8);
+int inputNumeroIntero = InputInt();
+double inputNumeroConLaVirgola = InputDouble();
+string inputfrase = Inserimentofrase();
 
-int InputInt ()
+Console.WriteLine($"Input nel range: {inputSceltaMenu}");
+Console.WriteLine($"input numero intero: {inputNumeroIntero}");
+Console.WriteLine($"Input con la virgola: {inputNumeroConLaVirgola:F2}");
+Console.WriteLine($"Input frase: {inputfrase}");
+
+int InputInt()
 {
     int numero = 0;
     bool repeat = false;
@@ -18,42 +22,60 @@ int InputInt ()
         repeat = false;
         try
         {
-            Console.Write("intero > ");
+            Console.Write("Inserisci numero intero > ");
             numero = int.Parse(Console.ReadLine());
         }
         catch (Exception ex)
         {
-            Console.WriteLine("X - Errore inserimento.");
+            Console.WriteLine("#Errore: dato non corretto");
             repeat = true;
         }
-    }while (repeat);
-    //Console.WriteLine("[v] Intero inserito.");
+    } while (repeat);
     return numero;
 }
 
-int NumberInRange (int min, int max)
+int NumberInRange(int min, int max)
 {
-    Console.WriteLine($"Inserisci intero tra {min} e {max}");
     bool repeat = false;
-    int num; 
+    int num = 0;
+    Console.Write($"Inserisci intero tra {min} e {max} ");
     do
     {
-        num = InputInt();
+        do
+        {
+            Console.Write("> ");
+            repeat = false;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("#Errore: dato non corretto");
+                repeat = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("#Errore: dato non corretto");
+                repeat = true;
+            }
+        } while (repeat);
+
         if (num >= min && num <= max)
         {
-            Console.WriteLine("*Numero nel range corretto*");
+            //Console.WriteLine("*Numero nel range corretto*");
             return num;
         }
         else
         {
-            Console.WriteLine ("X - Variabile fuori dal range");
+            Console.WriteLine("#Errore: numero fuori dal range");
             repeat = true;
         }
     } while (repeat);
     return -1;
 }
 
-double InputDouble ()
+double InputDouble()
 {
     double numero = 0;
     bool repeat = false;
@@ -61,53 +83,45 @@ double InputDouble ()
     do
     {
         repeat = false;
-        
-            Console.Write("Inserisci numero decimale > ");
-            s_numero = Console.ReadLine();
-            s_numero = s_numero.Replace(".",",");
 
-            try
-            {
-                numero = Convert.ToDouble(s_numero);
-            }
-            catch (Exception e)
-            {
-              Console.WriteLine("non convertito");
-              repeat = true;
-            }
+        Console.Write("Inserisci numero decimale > ");
+        s_numero = Console.ReadLine();
+        s_numero = s_numero.Replace(",", ".");
 
+        try
+        {
+            numero = Convert.ToDouble(s_numero);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("#Errore: dato non corretto");
+            repeat = true;
+        }
+    } while (repeat);
 
-            //numero = double.Parse(Console.ReadLine());
-        
-        // catch (Exception ex)
-        // {
-        //     repeat = true;
-        // }
-    }while (repeat);
-
-    Console.WriteLine("*Decimale insierito*");
+    //Console.WriteLine("*Decimale insierito*");
     return numero;
 }
 
-string Inserimentofrase ()
+string Inserimentofrase()
 {
 
-    Console.WriteLine("Inserisci una stringa: ");
+    Console.Write("Inserisci una stringa > ");
     string frase;
     frase = Console.ReadLine();
     bool ripeti = false;
     do
     {
-        ripeti = string.IsNullOrWhiteSpace (frase);
+        ripeti = string.IsNullOrWhiteSpace(frase);
 
         if (ripeti) // if true
         {
-            Console.WriteLine ("X - Stringa vuota. Reinserire");
+            Console.WriteLine("#Errore: stringa vuota");
             frase = Console.ReadLine();
         }
         else
         {
-            Console.WriteLine ("*Stringa inserita*");
+            //Console.WriteLine("*Stringa inserita*");
             return frase;
         }
     } while (ripeti);
