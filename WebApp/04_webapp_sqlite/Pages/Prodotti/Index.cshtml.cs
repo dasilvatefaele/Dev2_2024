365390c5d3830f1ec139db4d878f7c034ef75f0c
Felipe <dasilvatefaele@gmail.com>
2025-02-14 10:27:31 +0100

    WebApp - aggiunta filtro prezzo crescente-decrescente

--- WebApp/04_webapp_sqlite/Pages/Prodotti/Index.cshtml.cs
+++ WebApp/04_webapp_sqlite/Pages/Prodotti/Index.cshtml.cs
@@ -1,55 +1,89 @@
 namespace _04_webapp_sqlite.Prodotti;
 
-public class IndexProdottiModel : PageModel {
+public class IndexProdottiModel : PageModel
+{
     private readonly ILogger<PrivacyModel> _logger;
 
     #region Proprietà prodotti
-    public List<ProdottoViewModel>? Prodotti { get; set; } = new ();
+    public List<ProdottoViewModel>? Prodotti { get; set; } = new();
     public int totaleProdotti { get; set; }
     #endregion
 
-    public IndexProdottiModel(ILogger<PrivacyModel> logger) {
+    [BindProperty(SupportsGet = true)]
+    public int Ordine { get; set; } = -1;
+
+    public IndexProdottiModel(ILogger<PrivacyModel> logger)
+    {
         _logger = logger;
     }
 
-    public void OnGet() {
-
+    public void OnGet()
+    {
         // Creiamo e apriamo la connessione al database
-        using (var connection = DatabaseInitializer.GetConnection()){
-            // apriamo la connessione
-            connection.Open();
 
-            // Occorre creare una query di join con una LEFT JOIN tra la tabella Prodotti e la tabella Categorie
-            // Usiamo gli alias in SQLite per rendere più leggibile il codice. Useremo p per Prodotti e c per Categorie
-            var query = @"
+        if (Prodotti.Count == 0)
+        {
+            using (var connection = DatabaseInitializer.GetConnection())
+            {
+                // apriamo la connessione
+                connection.Open();
+
+                // Occorre creare una query di join con una LEFT JOIN tra la tabella Prodotti e la tabella Categorie
+                // Usiamo gli alias in SQLite per rendere più leggibile il codice. Useremo p per Prodotti e c per Categorie
+                var query = @"
                 SELECT p.Id, p.Nome, p.Prezzo, c.Nome as Categoria
                 FROM Prodotti p
                 LEFT JOIN Categorie c ON p.CategoriaId = c.Id
                 ORDER BY p.Nome";
 
-            // Creiamo il comando
-            using (var command = new SQLiteCommand(query, connection)) {
-                // Eseguiamo il comando
-                using (var reader = command.ExecuteReader()) {
-                    // Leggiamo i dati
-                    while (reader.Read()) {
-                        // Creiamo un nuovo prodotto e lo aggiungiamo alla lista
-                        Prodotti?.Add( new ProdottoViewModel {
-                            Id = reader.GetInt32(0),
-                            Nome = reader.GetString(1),
-                            Prezzo = reader.GetDouble(2),
-                            // se la categoria è nulla, restituiamo "Nessuna categoria"
-                            CategoriaNome = reader.IsDBNull(3) ? "Nessuna categoria" : reader.GetString(3)
-                        });
+                // Creiamo il comando
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    // Eseguiamo il comando
+                    using (var reader = command.ExecuteReader())
+                    {
+                        // Leggiamo i dati
+                        while (reader.Read())
+                        {
+                            // Creiamo un nuovo prodotto e lo aggiungiamo alla lista
+                            Prodotti?.Add(new ProdottoViewModel
+                            {
+                                Id = reader.GetInt32(0),
+                                Nome = reader.GetString(1),
+                                Prezzo = reader.GetDouble(2),
+                                // se la categoria è nulla, restituiamo "Nessuna categoria"
+                                CategoriaNome = reader.IsDBNull(3) ? "Nessuna categoria" : reader.GetString(3)
+                            });
+                        }
+                        // ordiniamo la lista secondo l'id
+
                     }
-                    // ordiniamo la lista secondo l'id
-                    Prodotti = Prodotti?.OrderBy(p => p.Id).ToList();
                 }
+                // Chiudiamo la connessione
+                // connection.Close();
+            };
+            if (Ordine == 0)
+            {
+
+                Prodotti = Prodotti?.OrderBy(p => p.Prezzo).ToList();
             }
-            // Chiudiamo la connessione
-            // connection.Close();
-        };
+            else if (Ordine == 1)
+            {
+                Prodotti = Prodotti?.OrderByDescending(p => p.Prezzo).ToList();
+            }
+            else
+            {
+                Prodotti = Prodotti?.OrderBy(p => p.Id).ToList();
+            }
+        }
     }
+
+    public IActionResult OnPost()
+    {
+
+        return RedirectToPage("Index", new { Ordine });
+    }
+
 }
 
 
