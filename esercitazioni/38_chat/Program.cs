38cd3746ffba0e88efe95c59dbc5d21db91d490b
Felipe <dasilvatefaele@gmail.com>
2025-02-21 10:50:06 +0100

    38_Chat e Chat_Server

---
+++ esercitazioni/38_chat/Program.cs
@@ -0,0 +1,59 @@
+﻿/* dipendenze: */
+
+using System.Net.Sockets;
+// da le funzionalità per la comunicazione in rete
+
+using System.Text;
+// da le funzionalità per la gestione delle stringhe che vengono inviate/ricevute
+
+public class Client
+{
+    // metodo per stabilire la connessione con il server
+    // argomenti: indirizzo ip del server e la porta
+    // su cui il server è in ascolto (cioè sulla quale opera). Usiamo il protocollo TCP
+
+    public void StartClient(string serverIP, int port)
+    {
+        using (var client = new TcpClient(serverIP, port))
+        // TcpClient è la classe che rappresenta un client TCP cioè un client che si connette a un server tramite protocollo TCP
+
+        using (var stream = client.GetStream())
+        // restituisce un oggetto NetworkStream
+        // che rappresenta il flusso di dati tra client e il server
+
+      
+        {
+            Console.WriteLine("Connesso al server.");
+            string messageToSend = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(messageToSend))
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(messageToSend);
+                // converte la stringa in un array di byte
+
+                stream.Write(buffer, 0, buffer.Length);
+                // serve indicare al server:
+                // memorizza il messaggio
+                // indice di partenza [0]
+                // indice di fine [buffer.Length]
+
+                messageToSend = Console.ReadLine();
+                // legge il messaggio da inviare
+            }
+        }
+    }
+
+    public static void Main (string[] args)
+    {
+        Client client = new Client();
+        // creo un'ostanza della classe Client 
+        // in modo da poter chiamare il metodo StartClient
+
+        Console.WriteLine("Inserici l'IP del server: ");
+        string serverIP = Console.ReadLine();
+        // chiedo l'indirizzo IP del server
+
+        client.StartClient(serverIP, 3000); 
+        // avvia il client con l'IP del server e la porta 3000
+    }
+}
---
+++ esercitazioni/38_chat_server/Program.cs
@@ -0,0 +1,100 @@
+﻿/* dipendenze: */
+
+using System.Net;
+using System.Net.Sockets;
+// da le funzionalità per la comunicazione in rete
+
+using System.Text;
+// da le funzionalità per la gestione delle stringhe 
+// che vengono inviate/ricevute
+
+using System.Threading; 
+// da le funzionalità per la gestione dei 
+// thread: flussi di esecuzione separati
+
+public class Server
+{
+    private TcpListener listener;
+    // oggetto che rappresenta un serve TCP
+
+    public void StartServer(int port)
+    {
+        listener = new TcpListener (IPAddress.Any, port);
+        // avrà bisogno degli IP che si connettono al server
+        // bisogna indicare quali IP vanno bene (oppure tutti gli IP)
+        // IPAddress.Any indica che il server accetta le connessioni
+        // su tutte le interfacce di rete
+
+        listener.Start();
+        // avvia il server
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            // accetta una connessione da un client e
+            // restituisce un oggetto TcpClient che rappresenta
+            // il client connesso
+
+            Thread clientThread = new Thread (new ParameterizedThreadStart(HandleClient));
+            // crea un nuovo thread per gestire il client connesso
+
+            clientThread.Start(client); 
+            // avvia il thread per gestire il client connesso, in questo caso thread
+        }
+    }
+
+    private void HandleClient (object obj)
+    {
+        TcpClient client = (TcpClient)obj; 
+        // converte l'oggetto passato come argomento 
+        // in un oggetto TcpClient
+
+        NetworkStream stream = client.GetStream();
+        // ottiene il flusso di dati tra l client e il server networkstream
+
+        byte[] buffer = new byte[1024];
+        // creiamo un buffer per convertire la stringa in byte
+
+        int byteCount; 
+        // memorizza il numero di byte che sono stati letti.
+        // while(byteCount != 0)
+
+        while ((byteCount = stream.Read(buffer, 0 , buffer.Length)) != 0)
+        {
+            string message = Encoding.ASCII.GetString(buffer, 0, byteCount);
+            // converto i byte in una stringa
+
+            Console.WriteLine($"Messaggio ricevuto: {message}");
+
+            Broadcast(message);
+            // Invia il messaggio a tutti i client connessi
+        }
+        client.Close();
+    }
+
+    private List<TcpClient> clients = new List<TcpClient>();
+
+    private void Broadcast(string message)
+    {
+        foreach (TcpClient client in clients) // per ogni client connesso
+        {
+            NetworkStream stream = client.GetStream();
+            // ottiene il flusso di dati tra client e server
+
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            // converte la stringa in byte
+
+           stream.Write(buffer, 0, buffer.Length);
+           // invia il messaggio al client
+        }
+    }
+
+    public static void Main (string[] args)
+    {
+        Server server = new Server();
+        // creo un'istanza della classe Server
+
+        server.StartServer(3000);
+        // avvio il server sulla porta 3000
+    }
+}
